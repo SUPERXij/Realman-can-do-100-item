e7d8c6f8db4005f73eef43c4019186540b7305d1
SUPERXij <86589033+SUPERXij@users.noreply.github.com>
2021-07-15 14:00:34 +0800

    Add files via upload

---
+++ xi_52.py
@@ -0,0 +1,6 @@
+# 学习使用按位或 | 。
+a = 0o77
+b = a | 3
+print('a | b is %d' % b)
+b |= 7
+print('a | b is %d' % b)
---
+++ xi_53.py
@@ -0,0 +1,6 @@
+# 学习使用按位异或 ^ 。
+a = 0o77
+b = a ^ 3
+print('The a ^ 3 = %d' % b)
+b ^= 7
+print('The a ^ b = %d' % b)
